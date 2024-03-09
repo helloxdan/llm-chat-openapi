8a966d5592d18b5234ae3dc33d89635a19a4d763
dahn <xuda@mogulinker.com>
2024-03-09 14:40:09 +0800

    fix

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='llm-chat-openapi',
+    version='1.0.5',
+    packages=find_packages(),
+    author='helloxdan',
+    author_email='hello.xdan@gmail.com',
+    description='chat api  collection for popular LLM',
+    long_description='chat api  collection for popular LLM',
+    long_description_content_type='text/markdown',
+    url='https://github.com/helloxdan/llm-chat-openapi',
+    install_requires=[
+        # List of dependencies
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
